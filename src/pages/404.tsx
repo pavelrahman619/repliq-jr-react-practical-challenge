58e0639f21269ee1e98b8aefc9bb0dc53d9fe8e4
pavelrahman619 <pavel.rahman@savethechildren.org>
2023-04-08 11:27:38 +0600

    remove @react/font, add custom 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+// pages/404.js
+import Link from 'next/link';
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <div>
+        <h1>404 - Page Not Found</h1>
+        <p>Sorry, there is nothing to see here</p>
+      </div>
+    </>
+  );
+};
+
+export default NotFoundPage;
