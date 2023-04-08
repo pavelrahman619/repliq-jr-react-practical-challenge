bdde8f5f2681d7a80a79eb077d6946ba12e741b8
pavelrahman619 <pavel.rahman@savethechildren.org>
2023-04-08 10:01:14 +0600

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
+import { Lato } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
+const inter = Lato({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${inter.className} prose`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
