f0901cd24eb961c27b4cd573b3728b7e61c5e574
pavelrahman619 <pavel.rahman@savethechildren.org>
2023-04-09 23:27:11 +0600

    fix build

--- src/pages/ecom/components/ProductCard.tsx
+++ src/pages/ecom/components/ProductCard.tsx
@@ -5,65 +5,66 @@ import {
     CardFooter,
     Typography,
     Button,
-  } from "@material-tailwind/react";
-  import Link from "next/link";
-  import { useRouter } from "next/router"; // Fix import for useRouter
-  import React from 'react';
-  
-  type Product = {
+} from "@material-tailwind/react";
+import Link from "next/link";
+import router from "next/router";
+import React from 'react';
+
+type Product = {
     id: number;
     title: string;
     description: string;
     price: number;
     image: string;
     // Add other properties as needed
-  };
-  
-  type ProductCardProps = {
-    product: Product;
+};
+
+type ProductCardProps = {
+    product: Product | undefined; // Make product prop optional and allow undefined
     handleAddToCart: (product: Product) => void;
     handleProductDetails: (product: Product) => void;
-  };
-  
-  const ProductCard = ({ product, handleAddToCart }: ProductCardProps) => {
-    const router = useRouter(); // Fix initialization of router
-  
+};
+
+const ProductCard = ({ product, handleAddToCart }: ProductCardProps) => {
+    // Add error handling for undefined product prop
+    if (!product) {
+        return <div>Error: Product not found</div>;
+    }
+
     const handleProductDetailsClick = () => {
-      // Use router to navigate to product-details page with product as query parameter
-      router.push({
-        pathname: '/ecom/product-details',
-        query: { productId: product.id, productTitle: product.title, productDescription: product.description } // Pass the product details as query parameters
-      });
+        // Use router to navigate to product-details page with product as query parameter
+        router.push({
+            pathname: '/ecom/product-details',
+            query: { productId: product.id, productTitle: product.title, productDescription: product.description } // Pass the product details as query parameters
+        });
     };
-  
-  
+
     return (
-      <Card key={product.id} className="w-96">
-        <CardHeader color="blue" className="relative h-56">
-          <img
-            src={`${product.image}`}
-            alt={`${product.title}`}
-            className="h-full w-full"
-          />
-        </CardHeader>
-        <CardBody className="text-center">
-          <Typography variant="h5" className="mb-2">
-            {product.id}: {product.title}
-          </Typography>
-          <Typography variant="h6">
-            Price: ${product.price}
-          </Typography>
-        </CardBody>
-        <CardFooter divider className="flex items-center justify-center py-3">
-          <Link href="/ecom/product-details">
-            <Button className="mr-2" onClick={handleProductDetailsClick}>Product Details</Button>
-          </Link>
-  
-          <Button onClick={() => handleAddToCart(product)}>Add to Cart</Button>
-        </CardFooter>
-      </Card>
+        <Card key={product.id} className="w-96">
+            <CardHeader color="blue" className="relative h-56">
+                <img
+                    src={`${product.image}`}
+                    alt={`${product.title}`}
+                    className="h-full w-full"
+                />
+            </CardHeader>
+            <CardBody className="text-center">
+                <Typography variant="h5" className="mb-2">
+                    {product.id}: {product.title}
+                </Typography>
+                <Typography variant="h6">
+                    Price: ${product.price}
+                </Typography>
+            </CardBody>
+            <CardFooter divider className="flex items-center justify-center py-3">
+                <Link href="/ecom/product-details">
+                    <Button className="mr-2" onClick={handleProductDetailsClick}>Product Details</Button>
+                </Link>
+
+                <Button onClick={() => handleAddToCart(product)}>Add to Cart</Button>
+            </CardFooter>
+        </Card>
     );
-  };
-  
-  export default ProductCard;
-  
+};
+
+export default ProductCard;
